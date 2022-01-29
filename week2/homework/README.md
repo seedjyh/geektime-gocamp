428d9d31a85f8038036cd849a36fea2ac8dcaad2
jyh <seedjyh@gmail.com>
2022-01-29 19:00:14 +0800

    move practice package under week*/practice/

---
+++ week2/homework/README.md
@@ -0,0 +1,12 @@
+# Week 2 Homework
+
+## Problems
+
+1. 我们在数据库操作的时候，比如 dao 层中当遇到一个 sql.ErrNoRows 的时候，是否应该 Wrap 这个 error，抛给上层。为什么，应该怎么做请写出代码？
+
+以上作业，要求提交到自己的 GitHub 上面，然后把自己的 GitHub 地址填写到班班提供的表单中： https://jinshuju.net/f/Om7xH6
+作业提交截止时间为 1 月 30 日（周日）晚 24:00 分前。
+
+## Answers
+
+
---
+++ week2/homework/main.go
@@ -0,0 +1,23 @@
+package main
+
+// mysql table definition
+//
+//mysql> desc student;
+//+-------+-------------+------+-----+---------+----------------+
+//| Field | Type        | Null | Key | Default | Extra          |
+//+-------+-------------+------+-----+---------+----------------+
+//| id    | bigint      | NO   | PRI | NULL    | auto_increment |
+//| name  | varchar(32) | YES  |     | NULL    |                |
+//| age   | int         | YES  |     | NULL    |                |
+//+-------+-------------+------+-----+---------+----------------+
+//
+//mysql> select * from student;
+//+----+---------+------+
+//| id | name    | age  |
+//+----+---------+------+
+//|  1 | seedjyh |   38 |
+//+----+---------+------+
+
+func main() {
+
+}
