cea6f12a63a9bfef990138b3e12df2d6c7e50255
jyh <jyh@eastcom.com>
2022-05-07 17:05:48 +0800

    remove test-seelog because it's in anh/pkg/mylog

--- final/test-seelog/go.mod
+++
@@ -1,8 +0,0 @@
-module test-seelog
-
-go 1.16
-
-require (
-	github.com/MenInBack/seelog v0.0.0-20171017071608-198f4d221391
-	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575 // indirect
-)
--- final/test-seelog/mylog/receiver.go
+++
@@ -1,7 +0,0 @@
-package mylog
-
-type Receiver interface {
-	Info(tags Tags, fields Fields, message Message)
-	Debug(tags Tags, fields Fields, message Message)
-	Error(tags Tags, fields Fields, message Message) error
-}
